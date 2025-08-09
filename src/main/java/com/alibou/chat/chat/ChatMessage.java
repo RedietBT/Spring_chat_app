38fbc5c472f0b46dd837f7f294c093f975180d61
unknown <redietberhanu2@gmail.com>
2025-08-09 11:38:25 +0300

    websocateEventListner class done

--- src/main/java/com/alibou/chat/chat/ChatMessage.java
+++ src/main/java/com/alibou/chat/chat/ChatMessage.java
@@ -1,39 +1,16 @@
 package com.alibou.chat.chat;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Getter
-@Setter
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class ChatMessage {
-
     private String content;
     private String sender;
     private MessageType type;
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getSender() {
-        return sender;
-    }
-
-    public void setSender(String sender) {
-        this.sender = sender;
-    }
-
-    public MessageType getType() {
-        return type;
-    }
-
-    public void setType(MessageType type) {
-        this.type = type;
-    }
 }
